c661dd6b580411b7398d58332c50026234836e88
jnrchry <jnrchry@gmail.com>
2023-01-03 18:08:16 +0000

    commit

--- src/pages/scores/Scores.js
+++ src/pages/scores/Scores.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./scores.scss";
 import Navbar from "../../components/navbar/Navbar";
+import Main from "../../components/main/Main";
 
 const Scores = () => {
   return (
     <div>
       <Navbar />
+      <Main />
     </div>
   );
 };
